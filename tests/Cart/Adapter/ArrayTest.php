01d0631b8b61256860e67d1b56c5ce1957b286a7
Erik Figueiredo <falecom@erikfigueiredo.com.br>
2015-09-17 17:38:33 -0300

    Primeiro commit

---
+++ tests/Cart/Adapter/ArrayTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace CakePhpBrasil\Cart\Adapter;
+
+use CakePhpBrasil\Cart\Adapter\ArrayAdapter;
+
+class ArrayTest extends \PHPUnit_Framework_TestCase
+{
+	protected $product_one = [
+		'id'=>1,
+		'title'=>'Celular MotoG 3',
+		'qtd'=>2,
+		'value'=>799.99
+	];
+
+	protected $product_two = [
+		'id'=>2,
+		'title'=>'Game Playstation 5',
+		'qtd'=>1,
+		'value'=>4599.99
+	];
+
+	public function testAdicionarItem()
+	{
+		$array = new ArrayAdapter();
+		$cart = $array->add($this->product_one);
+
+		$expected = [
+			$this->product_one
+		];
+
+		$this->assertEquals($expected, $cart);
+	}
+
+	public function testAdicionarMultiplosItens()
+	{
+		$array = new ArrayAdapter();
+		$array->add($this->product_one);
+		$cart = $array->add($this->product_two);
+
+		$expected = [
+			$this->product_one,
+			$this->product_two
+		];
+
+		$this->assertEquals($expected, $cart);
+	}
+
+	public function testRemovendoItem()
+	{
+		$array = new ArrayAdapter();
+		$array->add($this->product_one);
+		$array->add($this->product_two);
+
+		$cart = $array->remove(1);
+
+		$expected = [
+			$this->product_two
+		];
+
+		$this->assertEquals($expected, $cart);
+	}
+}
