54827ac475dbc2f38672edc93708bbcaa7734519
Erik Figueiredo <falecom@erikfigueiredo.com.br>
2015-09-18 17:44:08 -0300

    Criado Singleton

---
+++ src/Cart/Cart.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace CakePhpBrasil\Cart;
+
+use CakePhpBrasil\Cart\Adapter\AdapterFactory;
+
+class Cart
+{
+    private static $adapter;
+    private static $instance;
+
+    public static function configure(AdapterFactory $adapter)
+    {
+        static::$adapter = $adapter;
+    }
+    
+    public static function get()
+    {
+        if (null === static::$instance) {
+            static::$instance = new CartBr(static::$adapter);
+        }
+        
+        return static::$instance;
+    }
+
+    protected function __construct()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+
+    private function __wakeup()
+    {
+    }    
+}
---
+++ tests/Cart/CartBrTest.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace CakePhpBrasil\Cart;
+
+use CakePhpBrasil\Cart\Adapter\ArrayAdapter;
+use CakePhpBrasil\Cart\CartBr as Cart;
+
+class CartBrTest extends \PHPUnit_Framework_TestCase
+{
+    protected $product_one = [
+        'id'=>1,
+        'title'=>'Celular MotoG 3',
+        'qtd'=>2,
+        'value'=>799.99
+    ];
+
+    protected $product_two = [
+        'id'=>2,
+        'title'=>'Game Playstation 5',
+        'qtd'=>1,
+        'value'=>4599.99
+    ];
+
+    protected $product_tree = [
+        'id'=>3,
+        'title'=>'Notebook Lenovo G400s',
+        'qtd'=>8,
+        'value'=>1499.99
+    ];
+
+    public function testAdicionandoProdutoNoCarrinho()
+    {
+        $cart = new Cart(new ArrayAdapter);
+        $cart->add($this->product_one);
+
+        $expected = [
+            $this->product_one
+        ];
+
+        $this->assertEquals($expected, $cart->all());
+    }
+
+    public function testAdicionandoMultiplosProdutosNoCarrinho()
+    {
+        $cart = new Cart(new ArrayAdapter);
+        $cart->add($this->product_one)
+            ->add($this->product_two)
+            ->add($this->product_tree);
+
+        $expected = [
+            $this->product_one,
+            $this->product_two,
+            $this->product_tree
+        ];
+
+        $this->assertEquals($expected, $cart->all());
+    }
+
+    public function testRemovendoProdutosDoCarrinho()
+    {
+        $cart = new Cart(new ArrayAdapter);
+        $cart->add($this->product_one)
+            ->add($this->product_two)
+            ->add($this->product_tree);
+
+        $cart->delete(2);
+
+        $expected = [
+            $this->product_one,
+            $this->product_tree
+        ];
+
+        $this->assertEquals($expected, $cart->all());
+    }
+
+    public function testOrdenacaoDosProdutosPorValorMenor()
+    {
+        $cart = new Cart(new ArrayAdapter);
+        $cart->add($this->product_one)
+            ->add($this->product_two)
+            ->add($this->product_tree);
+
+        $cart->order(Cart::ORDER_BY_VALUE);
+
+        $expected = [
+            $this->product_one,
+            $this->product_tree,
+            $this->product_two
+        ];
+
+        $this->assertEquals($expected, $cart->all());
+    }
+
+    public function testOrdenacaoDosProdutosPorValorMaior()
+    {
+        $cart = new Cart(new ArrayAdapter);
+        $cart->add($this->product_one)
+            ->add($this->product_two)
+            ->add($this->product_tree);
+
+        $cart->order(Cart::ORDER_BY_VALUE, true);
+
+        $expected = [
+            $this->product_one,
+            $this->product_tree,
+            $this->product_two
+        ];
+
+        $expected = [
+            $this->product_two,
+            $this->product_tree,
+            $this->product_one
+        ];
+
+        $this->assertEquals($expected, $cart->all());
+    }
+}
