7a9d6aa29329736cf04ad1d2f6350040c3a73e28
Erik Figueiredo <falecom@erikfigueiredo.com.br>
2015-09-24 15:22:06 -0300

    Movido suporte a array_collumn para arquivo externo

--- src/Cart/Adapter/ArrayAdapter.php
+++ src/Cart/Adapter/ArrayAdapter.php
@@ -9,22 +9,23 @@ class ArrayAdapter implements AdapterFactory
     public function add(Array $product)
     {
         $key = $this->getKey($product['id']);
+
         if ($key === false) {
             $this->products = array_merge($this->products, [$product]);
         } else {
             $this->products[$key]['qtd'] += 1;
         }
+
         return $this;
     }
     
     public function delete($id)
     {
-        $key = $this->getKey($id);
         
-        if ($key !== false) {
+        if (($key=$this->getKey($id)) !== false) {
             $this->products[$key]['qtd'] -= 1;
 
-            $this->unsetProduct($key);
+            if ($this->products[$key]['qtd'] <= 0) unset($this->products[$key]);
         }
 
         return $this;
@@ -37,24 +38,8 @@ class ArrayAdapter implements AdapterFactory
 
     protected function getKey($id)
     {
-        return array_search($id, $this->array_column($this->products, 'id'));
+        return array_search($id, array_column($this->products, 'id'));
     }
 
-    protected function array_column($array,$column_name)
-    {
-        if(!function_exists("array_column")) {
-            return array_map(function($element)use($column_name){
-                return $element[$column_name];
-            }, $array);
-        }
-
-        return array_column($this->products, 'id');
-    }
-
-    private function unsetProduct($key)
-    {
-        if ($this->products[$key]['qtd'] <= 0) {
-            unset($this->products[$key]);
-        }
-    }
+    
 }
